554a0cd6a3611c7f2036877903629b984b45b494
kongxx <kongxx@845c0de6-5b2e-0410-a504-4d4ca1ea7471>
2012-01-31 00:12:38 +0000

    init CloneUtil class

---
+++ trunk/jutility-io/src/main/java/com/googlecode/jutility/io/CloneUtil.java
@@ -0,0 +1,46 @@
+package com.googlecode.jutility.io;
+
+import java.io.*;
+
+public class CloneUtil {
+	public static Object clone(Object obj) {
+		Object anotherObj = null;
+		byte[] bytes;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(obj);
+			bytes = baos.toByteArray();
+		} catch(IOException ex) {
+			throw new RuntimeException(ex.getMessage(), ex);
+		} finally {
+			if (oos != null) {
+				try {
+					oos.close();
+				} catch (IOException e) {
+					// ignore me
+				}
+			}
+		}
+		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+		ObjectInputStream ois = null;
+		try {
+			ois = new ObjectInputStream(bais);
+			anotherObj = ois.readObject();
+		} catch(IOException ex) {
+			throw new RuntimeException(ex.getMessage(), ex);
+		} catch(ClassNotFoundException ex) {
+			throw new RuntimeException(ex.getMessage(), ex);
+		} finally {
+			if (ois != null) {
+				try {
+					ois.close();
+				} catch (IOException e) {
+					// ignore me
+				}
+			}
+		}
+		return anotherObj;
+	}
+}
